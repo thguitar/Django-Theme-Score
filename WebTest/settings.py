8476ae39e6f7a03c5816e448b694d132b4a22fdf
JamesDBA <thiagol.guitarx@gmail.com>
2020-03-15 10:07:54 -0300

    Change settings.py

--- WebTest/settings.py
+++ WebTest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zk9o#++hs6wk=lf8nkb##_q)pvpx0v4$x=k@ohg6gtm+q!-y8b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-theme-score.herokuapp.com']
 
 
 # Application definition
